7a978abcba55ae98b11443184b4c43f0210ed309
Min Sheldon <minsheldon@Mins-MacBook-Air.local>
2022-12-01 15:14:08 +0000

    First version of first exercise code

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,11 +1,17 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+
+  return (sandwich['fillings']);
+
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+
+  return (person['city']=== 'Manchester');
+  
 }
 
 export function getBusNumbers(people) {
