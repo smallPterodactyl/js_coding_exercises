bde83c0e52a33410730159c3a20953b1545807ce
Min Sheldon <minsheldon@Mins-MacBook-Air.local>
2022-12-02 09:37:54 +0000

    Commit of first and second challenge files; delays due to push issues

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -11,20 +11,36 @@ export function isFromManchester(person) {
   // Your code here!
 
   return (person['city']=== 'Manchester');
-  
+
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+
+  //This value may need to be re-set in future
+  const maxPassengers = 40;
+
+  return (Math.ceil(people/maxPassengers));
+
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
-}
+  const sheepArr = arr.filter(animal => {
+    return animal === 'sheep';
+  });
+  return sheepArr.length;
+
+};
+
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  
+  return ((person['address']['postCode'][0]) === 'M' && 
+          (person['address']['city'] === 'Manchester'));
+
 }
