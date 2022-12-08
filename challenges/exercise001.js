4699428f7dc54218d973f9fc8bc0554fbceb27c9
Min Sheldon <minsheldon@Mins-MacBook-Air.local>
2022-12-08 13:36:51 +0000

    Refactored and cleaned up Exercise 1: * removed comments * replaced strings with template literals (generateInitials) * condensed conditional logic with ternary operators (addVAT, getSalePrice) * conensed recursive logic with iterators (reverseAllWords, countLinuxUsers)

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,8 +2,7 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-
+  
   return word[0].toUpperCase()+ word.slice(1);
 
 }
@@ -11,70 +10,57 @@ export function capitalize(word) {
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-
-  // Add your code here!
   
   //Capitalize as a precaution
-  return ((firstName[0]).toUpperCase() + '.' + (lastName[0]).toUpperCase());
+  // 8.12.22: Replaced strings with template literals
 
+  return `${firstName.substring(0,1).toUpperCase()}.${lastName.substring(0,1).toUpperCase()}`;
 }
 
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  
 
   let vatPrice = originalPrice * (1 + vatRate/100);
 
-  if (vatPrice.isInteger) {
-     return (vatPrice);
-  } 
-  else {
-     return (parseFloat(vatPrice.toFixed(2)));
-  }
+  // 8.12.22: Condensed logic into ternary operator
+  return vatPrice.isInteger ? vatPrice : parseFloat(vatPrice.toFixed(2));
 
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  
 
    let salePrice = originalPrice * (1 - reduction/100);
 
-   if (salePrice.isInteger) {
-     return salePrice;
-   }
+   // 8.12.22: Condensed logic into ternary operator
+   return salePrice.isInteger ? salePrice : parseFloat(salePrice.toFixed(2));
 
-   else {
-      return (parseFloat(salePrice.toFixed(2)));
-   }
  }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  
 
   if (str.length % 2 > 0) {
-
     //Case 1: An odd-numbered string
 
       let midPoint =  (str.length - 1) / 2;
       return str[midPoint];
     }
-
     //Case 2: An even-numbered string
-
     else {
         let midPoint = str.length/2;
         return (str[midPoint - 1] + str[midPoint]);
     }   
-
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  
 
   let newWord = '';
   for (let i = (word.length -1); i >= 0; i--) {
@@ -86,45 +72,26 @@ export function reverseWord(word) {
 
 export function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
-  let reversedWords = [];
 
-  // Process each word in the array, calling the previous function
-  for (let i = 0; i < words.length; i++) {
-   
-        let nextWord = reverseWord (words[i]);
-        reversedWords.push(nextWord);
-  }
+  // 8.12.22 Re-wrote using array map method
+  const reversedWords = words.map (w => reverseWord(w));
+  
   return reversedWords;
 }
 
 
 export function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  
+  // 8.12.22 Re-wrote using array filter method
+  // (note use of .length)
 
-  let countLinuxUsers = 0;
-  let _thisUser = [''];
-  let found = '';
-
-  for (const thisUser in users) {
-
-       //Loop through the object, counting only Linux users
-      _thisUser = (Object.values(users[thisUser]));
-
-      found = _thisUser.find(element => element === 'Linux');
-      if (found) {
-
-          countLinuxUsers += 1;
-
-      }  
-  } 
-  return countLinuxUsers;
+  return users.filter(u => u.type === 'Linux').length;
 }
 
 export function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
+  
 
   let sumScores = '';
   let meanScore = 0;
@@ -132,12 +99,11 @@ export function getMeanScore(scores) {
   sumScores = scores.reduce((accumulator, currentValue) => accumulator + currentValue);
   meanScore = parseFloat((sumScores/scores.length).toFixed(2));
   return (meanScore);
-
 }
 
 export function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
+  
 
   if (n%3 === 0){
     if (n%5 === 0) {
