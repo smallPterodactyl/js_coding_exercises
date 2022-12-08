d73e777d35ec63d145bab560ab5ff69e44a563d2
Min Sheldon <minsheldon@Mins-MacBook-Air.local>
2022-12-08 17:01:20 +0000

    Refactored and cleaned up Exercise 2: *cleaned up comments * removed direct references to object properties * handled potential 'undefined' (hasMPostCode) * condensed filter function (countSheep)

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,46 +1,45 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
 
-  return (sandwich['fillings']);
+  //8.12.22 Rewrote, removing bracket notation to avoid referencing properties
+
+  return sandwich.fillings;
 
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
 
-  return (person['city']=== 'Manchester');
+  //8.12.22 Rewrote, removing bracket notation to avoid referencing properties
+
+  return person.city === 'Manchester';
 
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
 
   //This value may need to be re-set in future
-  const maxPassengers = 40;
-
-  return (Math.ceil(people/maxPassengers));
+  
+  //8.12.22 Rewrote constatnt using shouting case syntax
+  
+  const MAX_PASSENGERS = 40;
+  return (Math.ceil(people/MAX_PASSENGERS));
 
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  const sheepArr = arr.filter(animal => {
-    return animal === 'sheep';
-  });
-  return sheepArr.length;
+
+ //8.12.22 Made this function more concise
+  return arr.filter(animal => animal === 'sheep').length;
 
 };
 
-
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
   
-  return ((person['address']['postCode'][0]) === 'M' && 
-          (person['address']['city'] === 'Manchester'));
+  return (person?.address?.postCode?.[0] === 'M') && 
+          (person?.address?.city === 'Manchester');
 
 }
