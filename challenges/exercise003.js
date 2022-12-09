fe07fb5313cfd292d9ce9fc3efa862b7f47b60ab
Min Sheldon <minsheldon@Mins-MacBook-Air.local>
2022-12-09 23:31:26 +0000

    Refactored getTotalSubjects and checkIngredients using feedback; also rewrote sortNumbers as a more concise function based on lessons learned and replacement of nested loops with two iterator functions

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,16 +1,13 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
 
-  const arrayOfSquares = nums.map((number) => Math.pow(number,2));
+  return nums.map((number) => Math.pow(number,2));
 
-  return arrayOfSquares;
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
-
+  
   //Process the initial word
   const firstWord = words[0];
    
@@ -37,69 +34,39 @@ export function camelCaseWords(words) {
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
-
-// This will need to be restrctured: it's not working 
-for (let i = 0; i < numStudents; i++) {
-
-  const numStudents = people[0].length;
-  let countSubjects = 0;
-
-      countSubjects += people[0][i]['subjects'].length;   
- }
   
- //This is not working in the test environment - scope issue to sort out
- return(countSubjects);
+  // 8.12.22 Solved after learning about new use for 'reduce'!
+
+  return people.reduce((accumulator, currentPerson) => 
+  accumulator += currentPerson.subjects.length, 0);
 
 }
   
-
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
-
-  let menus = Object.entries(menu);
-
-  //Set up a loop to examine all items on the menu
-  for (let i = 0; i < menus.length; i++) {
-     
-    // Pass each menu item's ingredients to an array to examine
-    let itemIngredients = ((menus[i][1]['ingredients']));
-    let found = itemIngredients.find(element => element === ingredient);
   
-    //If the target ingredient is found, we can exit
-    if (found) {
-      return true;
-    }   
-  }
-  // The loop has completed without finding the ingredient
-  return false;
+  //8.12.22 Rewrote, replacing loop with nested 'includes'
+
+  return (menu.find(menuItem => 
+         menuItem.ingredients.includes(ingredient)) !== undefined)
 
 }
+
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
 
-  let duplicateArray = [];
-  let matchFound = '';
+    /*
+   * Use Set to reduce one array to unique values; then
+   * filter it, returning only values found in the second array 
+   */
 
-  //For each element of arr1, try to find a match in arr2
-  for (let i = 0; i < arr1.length; i++) {
+  return sortNumbers([...new Set (arr1)]).filter(n => arr2.includes(n));
 
-    //Form a new array from any matched numbers
-    matchFound = arr2.find(element => element === arr1[i]);
-    if (matchFound) {
-      duplicateArray.push(matchFound);
-   } 
-  }
-  //Filter out duplicates
-   let uniqueDuplicates = duplicateArray.filter((element, index) => {
-   return duplicateArray.indexOf(element) === index;});
-
-   //Try to make the ascending order work ... but it isn't
-   return uniqueDuplicates.sort(function(a,b){return a-b});
-
+  // Function to perform numeric sorting
+    function sortNumbers(anyArray) {
+      return anyArray.sort((a, b) => a - b)
+    } 
 }
